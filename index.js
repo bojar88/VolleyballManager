0c995e28896617f28c4cce9deaf437af819f9dcf
bojar88 <marcin.bojarowski@gmail.com>
2025-02-01 15:32:39 +0100

    Fetch

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-alert("Hello log");
+let idMeczu = document.getElementById("id").value;
+let downloadButton = document.getElementById("download");
+
+downloadButton.addEventListener("click", () => {
+  pobierzDane();
+});
+
+function pobierzDane() {
+  const formData = new FormData();
+  formData.append("login", "bojar88");
+  formData.append("pass", "cXf@hRvd7e@psh@");
+  fetch(
+    "https://vm-manager.org/index.php?view=Login",
+
+    {
+      body: formData,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      method: "post",
+      mode: "no-cors",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    }
+  ).then((res) => {
+    console.log(res);
+  });
+}
